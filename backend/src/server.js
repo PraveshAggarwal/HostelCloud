1b0a0b0d0afdc445f19bef3360ea5a17ccbe3278
PraveshAggarwal <praveshaggarwal10@gmail.com>
2025-11-29 23:45:56 +0530

    pre final code

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,12 +18,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://hostelcloud-1.onrender.com"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 

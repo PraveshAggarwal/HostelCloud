64a8f13f76a05b63fb04fffca2d335833f906f5f
PraveshAggarwal <praveshaggarwal10@gmail.com>
2025-11-30 17:27:07 +0530

    final code

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // Routes
 import authRoutes from "./routes/auth.js";
@@ -18,22 +19,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://hostelcloud-1-frontend.onrender.com",
-  ],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -46,6 +39,14 @@ app.use("/api/complaints", complaintRoutes);
 app.use("/api/leaves", leaveRoutes);
 app.use("/api/laundry", laundaryRoutes);
 app.use("/api/notices", noticeRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
@@ -9,7 +10,7 @@ export const axiosInstance = axios.create({
 
 // Add token to requests if available
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
